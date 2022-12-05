a3d6a71d0b3d87e55119a83b3fc8aff864619314
roomaansar54 <roomaansar54@gmail.com>
2022-12-05 15:38:13 +0500

    commit

--- src/app/match-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MatchCountPipe } from './match-count.pipe';
-
-describe('MatchCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MatchCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
